815f7aa117a4d3120bb3cf47a4a6a26151fdd75b
tinayi08 <christinayi827@gmail.com>
2021-09-28 23:27:33 -0700

    updated ArrayList to hold all previous transactions

--- src/PreviousTXN.java
+++ src/PreviousTXN.java
@@ -5,13 +5,11 @@ public class PreviousTXN {
 
     private int amount;
     private String action;
-    public static ArrayList <PreviousTXN> transactionList = new ArrayList<>();
-
+    private static ArrayList <PreviousTXN> transactionList = new ArrayList<>();
 
     public PreviousTXN() {
 
     }
-
 
     public PreviousTXN(int amount, String action) {
         this.amount = amount;
@@ -22,27 +20,25 @@ public class PreviousTXN {
         PreviousTXN newTXN = new PreviousTXN(amount, action);
 
         transactionList.add(newTXN);
+
     }
 
     public void statement() {
 
-        if (this.action == null) {
-            System.out.println("You have not made any transactions yet.");
-            return;
-        }
-//        } else if (this.action.equalsIgnoreCase("deposit")) {
-//            System.out.println("You have deposited $" + amount + " into your account today.");
-//        } else if (this.action.equalsIgnoreCase("withdraw")) {
-//            System.out.println("You have withdrawn $" + amount + " from your account today.");
-//        }
-
         for (PreviousTXN t : transactionList) {
-            if (this.action.equalsIgnoreCase("deposit")) {
-                System.out.println("You have deposited $" + amount + " into your account today.");
-            } else if (this.action.equalsIgnoreCase("withdraw")) {
-                System.out.println("You have withdrawn $" + amount + " from your account today.");
+            if (t.action.equalsIgnoreCase("deposit")) {
+
+                System.out.println("You have deposited $" + t.amount + " into your account today.");
+            } else if (t.action.equalsIgnoreCase("withdraw")) {
+                System.out.println("You have withdrawn $" + t.amount + " from your account today.");
             }
+
         }
+
+        if (transactionList == null) {
+            System.out.println("You have not made any transactions yet.");
+        }
+
     }
 
 
--- src/Transaction.java
+++ src/Transaction.java
@@ -14,28 +14,27 @@ public class Transaction {
     }
 
     public void makeTransaction(User user, String action) {
-
-        previousTXN = howMuchToMove(action, user.getBalance());
+        howMuchToMove(action, user.getBalance());
+        //previousTXN = howMuchToMove(action, user.getBalance());
         user.setBalance(updatedBalance(user.getBalance(), action));
-        //previousTXN.transactionList.add(previousTXN);
-        //return amount;
+
     }
 
 
 
 
-    private PreviousTXN howMuchToMove(String action, int currentBal) {
+    //private PreviousTXN howMuchToMove(String action, int currentBal) {
+    private void howMuchToMove(String action, int currentBal) {
         System.out.println("How much would you like to " + action + " today?");
         if (action.equalsIgnoreCase("deposit")) {
             amount = validator.amountToDeposit();
         } else
             amount = validator.amountToWithdraw(currentBal);
-
         previousTXN.addToTransactionList(amount,action);
-        PreviousTXN newTXN = new PreviousTXN(amount, action);
+        //PreviousTXN newTXN = new PreviousTXN(amount, action);
 
-        return newTXN;
-        //return amount;
+        //return newTXN;
+
     }
 
 
